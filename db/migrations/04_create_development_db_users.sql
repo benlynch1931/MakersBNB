b0dab0a1213bc9336e130228fde86439f2f33b2e
David Millen <70208036+dm-devtech@users.noreply.github.com>
2020-10-29 11:53:03 +0000

    Added migrations files for user table on test and live databases

---
+++ db/migrations/04_create_development_db_users.sql
@@ -0,0 +1,26 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(50),
+  last_name VARCHAR(50),
+  email VARCHAR(50),
+  tel_number INT,
+  password VARCHAR(140)
+);
+
+INSERT INTO users(first_name, last_name, email, tel_number, password)
+VALUES(
+  'John',
+  'Smith',
+  'test@example',
+  01647475367,
+  'johnspassword1234'
+);
+
+INSERT INTO users(first_name, last_name, email, tel_number, password)
+VALUES(
+  'Barry',
+  'Johnson',
+  'test@example',
+  01333748463,
+  'barryspassword1234'
+);
