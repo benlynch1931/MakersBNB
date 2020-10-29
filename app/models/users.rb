458c3f7b2638bef6b6b5334c735e962b141dc4ca
Ben <ben.lynch1931@gmail.com>
2020-10-29 17:40:08 +0000

    Added BCrypt to files

--- app/models/users.rb
+++ app/models/users.rb
@@ -3,9 +3,9 @@ require 'bcrypt'
 
 class User
   def self.create(first_name:, last_name:, email:, phone_no:, password:)
-    # encrypted_password = BCrypt::Password.create(password)
+    encrypted_password = BCrypt::Password.create(password)
     connection = PG.connect(dbname: 'makers_bnb_manager_test')
-    result = connection.query("INSERT INTO users (first_name, last_name, email, phone_no, password) VALUES('#{first_name}', '#{last_name}', '#{email}', '#{phone_no}', '#{password}') RETURNING id, email;")
+    result = connection.query("INSERT INTO users (first_name, last_name, email, phone_no, password) VALUES('#{first_name}', '#{last_name}', '#{email}', '#{phone_no}', '#{encrypted_password}') RETURNING id, email;")
     User.new(id: result[0]['id'], first_name: result[0]['first_name'], last_name: result[0]['last_name'], email: result[0]['email'])
   end
 
@@ -14,10 +14,8 @@ class User
     connection = PG.connect(dbname: 'makers_bnb_manager_test')
     result = connection.query("SELECT * FROM users WHERE email = '#{email}'")
     return nil unless result.any?
-    # decrypt =  BCrypt::Password.new(result[0]['password'])
-    # puts decrypt
-    return nil if password != result[0]['password']
-    User.new(id: result[0]['id'], first_name: result[0]['first_name'], last_name: result[0]['last_name'], email: result[0]['email'])
+    return unless BCrypt::Password.new(result[0]['password']) == password
+      User.new(id: result[0]['id'], first_name: result[0]['first_name'], last_name: result[0]['last_name'], email: result[0]['email'])
 
   end
 
