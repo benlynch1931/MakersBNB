cd63e9bcb2e8142f74f77d289bf484695cb6c02b
David Millen <70208036+dm-devtech@users.noreply.github.com>
2020-10-29 14:58:17 +0000

    Created .find user method.

--- app/models/users.rb
+++ app/models/users.rb
@@ -3,10 +3,18 @@ require 'pg'
 class User
   def self.create(email:, password:)
     connection = PG.connect(dbname: 'makers_bnb_manager_test')
-    connection.query("INSERT INTO users (email, password) VALUES('#{email}', '#{password}') RETURNING id, email;")
+    result = connection.query("INSERT INTO users (email, password) VALUES('#{email}', '#{password}') RETURNING id, email;")
     User.new(id: result[0]['id'], email: result[0]['email'])
   end
-      attr_reader :id, :email
+
+  def self.find(id)
+    return nil unless id
+    connection = PG.connect(dbname: 'makers_bnb_manager_test')
+    result = connection.query("SELECT * FROM users WHERE id = '#{id}'")
+    User.new(result[0]['id'], result[0]['email'])
+  end
+
+  attr_reader :id, :email
 
   def initialize(id:, email:)
     @id = id
--- spec/unit/users_spec.rb
+++ spec/unit/users_spec.rb
@@ -7,14 +7,29 @@ describe User do
   before (:each) do
     reset_table
   end
-  
+
   describe '.create' do
     it 'creates a new user' do
       users = User.create(email: 'josh@example.com', password: 'poaqwes')
-      persisted_data = persisted_data(table: :users, id: user.id)
+      persisted_data = persisted_data(table: :users, id: users.id)
       expect(users).to be_a User
       expect(users.id).to eq persisted_data.first['id']
       expect(users.email).to eq 'josh@example.com'
     end
   end
+
+describe '.find' do
+  it 'finds a user by ID' do
+    user = User.create(email: 'test@example.com', password: 'password123')
+    result = User.find(id: user)
+
+    expect(result.id).to eq user.id
+    expect(result.email).to eq user.email
+  end
+
+  it 'returns nil if there is no ID given' do
+    expect(User.find(nil)).to eq nil
+  end
+end
+
 end
